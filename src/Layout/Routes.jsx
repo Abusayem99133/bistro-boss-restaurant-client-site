a30aae932cb96c54dbe7c29c88b2759c200ae8e7
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-05-16 03:29:36 +0600

    single page application tailwind and daysi ui and react router and react carousel responsive install and setup done

---
+++ src/Layout/Routes.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "./Main";
+import Home from "../Pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: "",
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Banner from "./Banner";
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <Banner></Banner>
+      </div>
+      <h1>This is Home</h1>
+    </div>
+  );
+};
+
+export default Home;
