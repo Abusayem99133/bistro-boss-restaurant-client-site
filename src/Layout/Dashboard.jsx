535ac077a7e3444b4c12151ef0c8231005c38eb8
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-05-20 17:40:19 +0600

    server site to client site delete function work done

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,12 +1,19 @@
-import { FaCalendar, FaHome, FaShoppingCart, FaWallet } from "react-icons/fa";
-import { FaBookBookmark } from "react-icons/fa6";
+import {
+  FaCalendar,
+  FaHamburger,
+  FaHome,
+  FaInbox,
+  FaShoppingCart,
+  FaWallet,
+} from "react-icons/fa";
+import { FaBookBookmark, FaMessage, FaShop } from "react-icons/fa6";
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
   return (
     <div className="flex">
       {/* dashboard site bar */}
-      <div className="w-64 min-h-screen bg-orange-400">
+      <div className="w-64 min-h-screen bg-[#97783b]">
         <ul className="menu p-4">
           <li>
             <NavLink to="/dashboard/userHome">
@@ -50,10 +57,35 @@ const Dashboard = () => {
               My Booking
             </NavLink>
           </li>
+          <div className="divider"></div>
+          <li>
+            <NavLink to="/">
+              <FaHome />
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/menu">
+              <FaHamburger />
+              Menu
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/shop/salad">
+              <FaShop />
+              Shop
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">
+              <FaInbox />
+              Contact
+            </NavLink>
+          </li>
         </ul>
       </div>
       {/* dashboard content */}
-      <div className="flex-1">
+      <div className="flex-1 p-8">
         <Outlet></Outlet>
       </div>
     </div>
--- src/Pages/DashboardCart/Cart.jsx
+++ src/Pages/DashboardCart/Cart.jsx
@@ -1,7 +1,91 @@
+import { Link } from "react-router-dom";
+import useCart from "../../Hooks/useCart";
+import { RiDeleteBinLine } from "react-icons/ri";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+
 const Cart = () => {
+  const [cart, refetch] = useCart();
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+  const axiosSecure = useAxiosSecure();
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/carts/${id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
   return (
     <div>
-      <h2 className="text-6xl">My Cart</h2>
+      <div className="flex items-center justify-evenly">
+        <h2 className="text-4xl">Items: {cart?.length}</h2>
+
+        <h2 className="text-4xl">Total Price:$ {totalPrice}</h2>
+        <Link>
+          <button className="btn btn-primary">Pay</button>
+        </Link>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="table ">
+          {/* head */}
+          <thead className="bg-orange-400">
+            <tr>
+              <th>#</th>
+              <th>Item Image</th>
+              <th>Item Name</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart?.map((item, index) => (
+              <tr key={item?._id}>
+                <th>{index + 1}</th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={item?.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>{item?.name}</td>
+                <td>${item?.price}</td>
+                <th>
+                  <button
+                    onClick={() => handleDelete(item?._id)}
+                    className="btn bg-red-600 text-white"
+                  >
+                    <RiDeleteBinLine />
+                  </button>
+                </th>
+              </tr>
+            ))}
+            {/* row 1 */}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
